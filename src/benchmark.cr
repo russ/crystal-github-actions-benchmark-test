dcdd50fe56789846814be5b680b7a472a149189d
Russ Smith <russ@bashme.org>
2025-06-10 16:08:52 -0700

    Testing

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -8,7 +8,7 @@ def fibonacci(n : Int32) : Int32
   n
 end
 
-Benchmark.ips(warmup: 1.seconds, calculation: 1.seconds) do |x|
+Benchmark.ips(warmup: 4.seconds, calculation: 10.seconds) do |x|
   x.report("fibonacci(10)") do
     fibonacci(10)
   end
