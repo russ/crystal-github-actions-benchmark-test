d049b1ae15aa1e1d7dac0d0c0c126302dcf7867c
Russ Smith <russ@bashme.org>
2025-06-11 08:38:13 -0700

    Testing

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -1,12 +1,8 @@
 require "benchmark"
 
 def fibonacci(n : Int32) : Int32
-  return 0 if n < 0
-  return n if n <= 1
-
-  (1..100_000_000).each { |i| Math.sqrt(i.to_f) }
-
-  fibonacci(n - 1) + fibonacci(n - 2)
+  sleep 1.second
+  return 0
 end
 
 Benchmark.ips(warmup: 4.seconds, calculation: 10.seconds) do |x|
